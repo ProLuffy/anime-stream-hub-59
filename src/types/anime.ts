57ca49f0102e2a2b2b4a53b66260cc2477a4128d
Pro Luffy <mmjoker786@gmail.com>
2026-02-25 17:55:24 +0530

    Create anime.ts

---
+++ src/types/anime.ts
@@ -0,0 +1,14 @@
+// /types/anime.ts
+export interface Anime {
+id: string;
+title: string;
+titleJp?: string;
+poster: string;
+description: string;
+genres: string[];
+type: 'anime' | 'donghua';
+status: 'ongoing' | 'completed' | 'upcoming';
+rating: number;
+year: number;
+languages: string[];
+} Ka naam toh bata
